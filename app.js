fcd7b8b3f0bb3b61cec36a331a621a4d8de05baa
Ana-Marija <anamaru1995@gmail.com>
2019-06-27 01:18:23 +0200

    'secondddddd'

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan'),
+const createError = require('http-errors'),
+    express = require('express'),
+    logger = require('morgan'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    cors = require('cors'),
     expressValidator = require('express-validator'),
-    cors = require('cors');
+    passport = require('passport'),
+    indexRouter = require('./routes/index'),
+    userRouter = require('./routes/user'),
+    messageRouter = require('./routes/message');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user');
-var messageRouter = require('./routes/message');
 
-
+// Connect to database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/teach_me',
     { useNewUrlParser: true , useCreateIndex: true })
     .then(()=> { console.log("Connected to database!"); }) //notify if success
     .catch(err => {
-      console.log(`Unable to connect to MongoDB!\nError: ${err}`); //notify of error that occurs
-      process.exit(1); //terminate program
+        console.log(`Unable to connect to MongoDB!\nError: ${err}`); //notify of error that occurs
+        process.exit(1); //terminate program
     });
 mongoose.Promise = global.Promise; //config mongoose promise to use global promise
 // Instantiate express
@@ -26,10 +25,6 @@ var app = express();
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator({
@@ -47,25 +42,22 @@ app.use(expressValidator({
         };
     }
 }));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.disable('etag');
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', passport.authenticate('jwt', { session: false }), userRouter);
+app.use('/message', passport.authenticate('jwt', { session: false }), messageRouter);
 
-app.use('/message', messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500).json({ msg: err.message });
 });
 
 module.exports = app;
